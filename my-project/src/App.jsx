6e98a7c49f7cbfdeb740f04c8cb96b654a8459a5
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-10-02 16:59:54 +0100

    Changed directory and added more paths

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DisplayMovie from "./components/DisplayMovie";
+import Movie from "./components/pages/Movie";
 import MovieDetails from "./components/MovieDetails"; // Import the new MovieDetailsPage component
+import TvShows from './components/pages/TvShows';
+import TvShowsDetails from "./components/TvShowsDetails";
+import WatchList from './components/pages/WatchList';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Route for the movie list */}
-        <Route path="/" element={<DisplayMovie />} />
-        {/* Route for the movie details page */}
+        <Route path="/" element={<Movie />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/tvshows" element={<TvShows />} />
+        <Route path="/tvshows/:id" element={<TvShowsDetails />} />
+        <Route path="/watchlist" element={<WatchList />} />
       </Routes>
     </Router>
   );
