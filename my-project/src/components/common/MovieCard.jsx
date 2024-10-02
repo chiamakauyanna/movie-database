84c67f54a441a468ea5f60498d33dd10b666d863
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-10-02 23:33:40 +0100

    Created a movie card

--- my-project/src/components/common/MovieCard.jsx
+++ my-project/src/components/common/MovieCard.jsx
@@ -1,25 +1,29 @@
-const MovieCard = ({ movie, onClick }) => {
+const MovieCard = ({
+  id,
+  poster_path,
+  title,
+  release_date,
+  vote_average,
+  onClick,
+}) => {
   return (
     <li
-      key={movie.id}
       className="rounded cursor-pointer flex flex-col"
-      onClick={() => onClick(movie.id)} // Navigate to details on click
+      onClick={() => onClick(id)} // Navigate to details on click
     >
       <img
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        alt={movie.title}
+        src={`https://image.tmdb.org/t/p/w200${poster_path}`}
+        alt={title || 'Poster'}
         loading="lazy"
         className="rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 duration-300"
       />
-      <h2 className="text-white font-medium py-3 w-52">{movie.title}</h2>
+      <h2 className="text-white font-medium py-3 w-52">{title}</h2>
       <div className="text-xs flex justify-between px-1">
-        <p className="text-gray-400 ">
-          {movie.release_date
-            ? new Date(movie.release_date).getFullYear()
-            : 'N/A'}
+        <p className="text-gray-400">
+          {release_date ? new Date(release_date).getFullYear() : 'N/A'}
         </p>
         <p className="text-yellow-500">
-          {movie.vote_average ? movie.vote_average.toFixed(1) : '0.0'}
+          {vote_average ? vote_average.toFixed(1) : '0.0'}
         </p>
       </div>
     </li>
