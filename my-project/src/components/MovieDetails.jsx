0c2b25060cac68a0e1f21109bea2ab09b06f3880
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-10-08 13:04:50 +0100

    Updated comonents with new changes

--- my-project/src/components/MovieDetails.jsx
+++ my-project/src/components/MovieDetails.jsx
@@ -1,23 +1,20 @@
 import { useParams } from 'react-router-dom';
-import MovieDetail from './common/MovieDetail';
-import Loading from './common/Loading';
+import ItemsDetailsList from './common/ItemsDetailsList';
 import useFetchDetails from '../hooks/useFetchDetails'
 
 const MovieDetails = () => {
   const { id } = useParams(); // Get the movie ID from the URL
   const { details: movie, cast, loading, error } = useFetchDetails('movie', id); // Use the hook for movies
 
-  if (loading) return <Loading />;
-
-  if (error) return (
-    <div className="w-screen flex justify-center my-28 h-screen">
-      <p className="text-red-500 w-72 text-center">{error}</p>
-    </div>
-  );
-
   return (
     <div className="container min-w-full">
-      <MovieDetail movie={movie} cast={cast} />
+      <ItemsDetailsList
+        title={movie ? movie.title : 'Movie Details'}
+        details={movie}
+        cast={cast}
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 };
