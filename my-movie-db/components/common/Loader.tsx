7d02afbc5735d961a484df8016dec137c2d3ea01
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-03-29 18:14:48 +0100

    styles: updated styling

--- my-movie-db/components/common/Loader.tsx
+++ my-movie-db/components/common/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center p-6">
+    <div className="flex h-screen items-center justify-center p-6">
       <div className="h-12 w-12 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
